74602413ba891fdd3bb9ec94cba11202a53766d8
guoziwei <guoziwei93@gmail.com>
2018-07-17 09:41:07 +0800

    调整细节

--- pages/geek/geek.js
+++ pages/geek/geek.js
@@ -13,7 +13,6 @@ Page({
     articles: [],
   },
     isSameDay: function(d1, d2) {
-      console.log(d1)
       return d1.getFullYear() === d2.getFullYear() &&
         d1.getMonth() === d2.getMonth() &&
         d1.getDate() === d2.getDate();
