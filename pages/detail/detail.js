328fc449c65ff0f2461d14021e0ae376706f7e85
guoziwei <guoziwei93@gmail.com>
2018-07-25 21:42:57 +0800

    增加md支持

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,4 +1,114 @@
 const app = getApp()
+
+const text = `> 本文由 [简悦 SimpRead](http://ksria.com/simpread/) 转码， 原文地址 https://www.ifanr.com/app/1069203
+
+前不久，苹果更新了 MacBook Pro 的产品线，为我们带来了全新的 2018 款 MacBook Pro。
+
+其实，相对于硬件，Mac 更让人津津乐道的是它的系统——macOS，以及为其提供的海量高质软件。常言道，磨刀不误砍柴工，一个强大的软件往往能让你在用电脑工作时事半功倍。
+
+今天，A 君根据 GitHub 上的开源软件项目，为大家整理了 50 多款 macOS 上最值得下载的免费软件合辑。准备好，一起用这些精品软件来武装你的 Mac 吧！
+
+### Pap.er
+
+相信很多人买了电脑之后第一件事就是换上自己喜欢的壁纸，毕竟原生的壁纸太丑了。还在百度图片上找壁纸？不如试试 Pap.er 吧。
+
+这款软件每天更新来自全球摄影师提供的精美壁纸。还会不定时推出一个主题，例如 A 君在写这篇稿时是「夏天」主题以及前段时间的「喵」等。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/bizhi111.jpg)
+
+另外 Pap.er 还很「轻便」，没有专门的应用界面，所有操作都是在状态栏展开的一个小窗中进行的。另外，比起其他只是帮你下载壁纸的软件更强大的是，Pap.er 不仅能下载，还能帮你设置，真正做到「一键换壁纸」的功能。
+
+那使用扩展屏的用户呢？不用急，Pap.er 上还提供了 4k 甚至 5k 的壁纸，以及最受码农欢迎的竖屏壁纸，真正把所有用户都照顾到了。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/bizhihengshu.jpg)
+
+### App Cleaner
+
+你以为 Mac 就不会卡了吗？太天真了。如果你长期不清理你电脑内的垃圾文件，等硬盘「余量告急」时你就知道错了。说到清理系统垃圾，最著名的应该就是「CleanMyMac」了，但是碍于它高昂的价格以及我们干瘪的荷包，最终不得不放弃。
+
+那就没办法了吗？不是的。今天 A 君就为大家带来一款免费的清理软件：App Cleaner。
+
+App Cleaner 本身的体积就很小，而且界面也很简洁，操作也很简单。打开 App Cleaner 后，你只需要将不用的软件图标拖进 App Cleaner 的框内，就可以自动帮你进行卸载，并清干净残留文件。
+
+养成定期清理不用软件的好习惯，向「卡机」说拜拜。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/sahnchu1.gif)
+
+### The Unarchiver
+
+macOS 其中一个很大的槽点就是如果不下载第三方的软件的话能解压的文件格式仅有. zip 一个，其他格式基本就不用想了。但是第三方解压缩软件这么多该怎么选？A 君今天就给你推荐一个：The Unarchiver。
+
+The Unarchiver 支持包括 RAR、ZIP、TAR、BZIP2、GZIP、7-ZIP 在内的主流压缩格式。而且使用方法也很简单，安装后只需要双击压缩文件就能自动解压。
+
+另外，他还有几项很贴心的可选功能，包括自动检测文件名编码（有效预防解压后乱码）、解压后自动删除源文件、为解压的文件创建新文件夹等。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/geshi.jpg)
+
+### IINA
+
+视频播放器千千万，为什么我独爱 IINA？
+
+始于颜值，终于才华。
+
+曾经的我有一个疑惑，就是为什么 macOS 这种颜值那么高的系统就一直找不到一个同样高颜值的视频播放软件。这个问题真的无解，直到遇见 IINA。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/INNAjietu.jpg)
+
+这是一款可以说是专门为了 macOS 而设的视频播放器。简洁的画面、大量的毛玻璃元素、4 款高质量的主题让人一见钟情。还是达不到你的要求？没关系，他还有大量的 UI 设置可以让你定制最适合自己的播放器。
+
+除了高颜值外，IINA 的实力也不俗。IINA 的播放核心是 mpv。简单来说，它不仅能播放几乎所有的视频格式，还能带来更稳定更快速的视频渲染。同时，它还为用户提供丰富的快捷键以及脚本系统，为用户提供了相当大的可玩空间。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/shezhi.jpg)
+
+如果你和 A 君一样，会为你的 MacBook 添置额外的屏幕和键盘的话，接下来的两款软件同样很适合你。
+
+### Karabiner Elements
+
+如果你正在使用 macOS，但是又不得不使用一个 Windows 布局的键盘时，又或者你像 A 君一样在用像 Minila Air 这样键位十分奇葩的键盘的话，这款改建神器相当适合你。
+
+正如上面所说，这是一款改键软件，最基础的功能就是设置按键映射了。有了它你想把空格改成删除、把删除改成回车都可以。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/gaijian.jpg)
+
+怎么用？拿 A 君的键盘做个例子，这个键盘的「OPTION」和「COMMAND」键的排列和苹果的键盘是相反的，那么 A 君只需要把「OPTION」投映成「COMMAND」，把「COMMAND」投映成「OPTION」，然后再把键帽换一下就可以了。
+
+另外，Karabiner Elements 还能存储多种修改方案，这对于拥有五花八门各种键盘的外设党就可以说相当实用了。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/gaijian2.jpg)
+
+### Lunar
+
+除了键盘外，不少用户还会为自己的 MacBook 添置一个甚至多个扩展屏幕。一般使用外扩屏幕都会遇到一个问题，就是调节亮度太麻烦了。
+
+在电脑上，按个「FN」+ 数字键就能解决的问题，但是在显示器上还得找那些小小个的实体按钮。有些显示器为了外观整洁，连个图标都省掉了，有时候按半天都没能把亮度调好，至于那些按钮在显示器背面的反人类设计就更不用说了。
+
+如果你有这样的问题，安装 Lunar 这款软件就是一个很好的解决方案。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/lunarjian.gif)
+
+首先 Lunar 最基础的功能就是可以在不影响笔记本屏幕的情况下对单个显示器的亮度进行调节，而且一切操作都是在桌面上完成的。
+
+你还能在 Lunar 上对特定的软件预设特定的亮度，例如工作用的软件可以适当暗一点，看电影玩游戏的时候可以适当亮一点。除此之外，你还可以在 Lunar 上选择随时间改变亮度，这样 Lunar 就会根据时间的变化为你推荐并更改屏幕亮度。
+
+### 还远不止这些
+
+除了上面所说的，开源软件社区 GitHub 上其实还有很多免费、实用又有趣的开源软件。另外，A 君也为大家整理了一份囊括了 50 个实用开源软件的清单，以下是清单的一部分：
+
+*   CAM：使用 iMac 或者 MacBook 的摄像头进行视频录制
+*   Tickeys：macOS 按键音，让你用蝶式键盘也能打出机械键盘的感觉。
+*   shallow-backup：轻松地创建轻量级的应用程序、安装包、字体等的备份
+*   Noto：具有可自定义主题的 macOS 文本编辑器
+*   Gifcurry：可以剪裁、添加文本等的 Gif 编辑器
+*   Imagine：PNG、JEPG 图片压缩工具
+*   Kawa：输入源切换器
+*   gfxCardStatus：独显 / 集显切换器
+*   Ao：界面优雅的 Todo 桌面应用
+*   Apple Juice：电池电量管理应用
+
+如果需要获取整个清单，请关注 AppSo 并回复「KY」，获取全部 macOS 上的免费软件。
+
+![](https://images.ifanr.cn/wp-content/uploads/2018/07/wpky.001-1024x640.jpeg)`;
+
 Page({
 
   /**
@@ -6,8 +116,10 @@ Page({
    */
   data: {
     url: "",
-    comments:[]
+    comments:[],
+    md: ""
   },
+
 
   /**
    * 生命周期函数--监听页面加载
@@ -15,7 +127,8 @@ Page({
   onLoad: function (options) {
     var that = this;
     that.setData({
-      url: `https://502tech.com/geekdaily/wxWebView?url=${options.url}`
+      url: `https://502tech.com/geekdaily/wxWebView?url=${options.url}`,
+      md: text
     })
     console.log(that.data.url);
   },
